84c7fc163fe79e6d7177ca0d0b52e0be67be9681
hungdaycb00 <60802128+hungdaycb00@users.noreply.github.com>
2021-01-06 10:15:59 +0700

    update

---
+++ MHBookStore/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHBookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ MHBookStore/Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHBookStore.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
